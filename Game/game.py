eb4fc847aced58b975bf66f557381d4a953ab0d9
Konrad Gadzina <k.gadzina@samsung.com>
2018-12-27 13:56:17 +0100

    Added roll method to game.py Added phases.py

--- Game/game.py
+++ Game/game.py
@@ -1,1 +1,10 @@
+from phases import Phases
 
+class Game:
+    environment_modifiers = {}
+    turn = -1
+    phase = Phases.MYTHOS
+
+    def roll(self, stat_name, stat_value, modifier):
+        result = stat_value + modifier + self.environment_modifiers.get(stat_name, 0)
+        return result if result > 0 else 0
---
+++ Game/phases.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Phases(Enum):
+    UPKEEP = 0
+    MOVEMENT = 1
+    ARKHAM_ENCOUNTER = 2
+    OTHER_WORK_ENCOUNTER = 3
+    MYTHOS = 4
